bb14be3674d7d1d92cc9d93c1d2dd9e8037d8c0c
Alexander Khrustalev <st.alex.ser@gmail.com>
2026-03-11 20:35:39 +0100

    Update lp.js

--- lp.js
+++ lp.js
@@ -1,5 +1,5 @@
 (function () {
     'use strict';
     // Загружаем основной движок поиска через асинхронный метод Lampa
-    Lampa.Utils.putScriptAsync(['https://raw.githubusercontent.com/alex-khrust/lp/main/tv_engine.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://raw.githubusercontent.com/alex-khrust/lp/main/tv_show.js'], function () { });
 })();
