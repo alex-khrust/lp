c04b6c791f6b5c9a876b6b2613d034c475c245a4
Alexander Khrustalev <st.alex.ser@gmail.com>
2026-03-11 21:28:29 +0100

    Update lp.js

--- lp.js
+++ lp.js
@@ -1,5 +1,7 @@
 (function () {
     'use strict';
-    // Загружаем движок через CDN, чтобы избежать Script error
-    Lampa.Utils.putScriptAsync(['https://cdn.jsdelivr.net/gh/alex-khrust/lp@main/tv_show.js'], function () { });
+    // Даем Lampa 500мс, чтобы инициализировать свои стандартные разделы
+    setTimeout(function() {
+        Lampa.Utils.putScriptAsync(['https://cdn.jsdelivr.net/gh/alex-khrust/lp@main/tv_show.js'], function () { });
+    }, 500);
 })();
