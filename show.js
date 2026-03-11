d97103592245ddeaede8553fec847faa5c710136
Alexander Khrustalev <st.alex.ser@gmail.com>
2026-03-11 17:19:55 +0100

    Update show.js

--- show.js
+++ show.js
@@ -5,13 +5,13 @@
         if (window.plugin_show_tv_ready) return;
         window.plugin_show_tv_ready = true;
 
-        // Создаем кнопку в меню
+        // Самый простой способ добавить пункт, который точно будет кликабельным
         Lampa.Menu.add({
             title: 'ТВ Шоу',
             icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z" fill="#32CD32"/></svg>',
             onSelect: function () {
                 Lampa.Input.edit({
-                    title: 'Название шоу (RU/UA)',
+                    title: 'Поиск ТВ-шоу',
                     value: '',
                     free: true
                 }, function (new_value) {
@@ -25,7 +25,7 @@
                     }
                 });
             }
-        });
+        }, 3); // Цифра 3 поставит пункт повыше в меню
     }
 
     if (window.appready) startPlugin();
