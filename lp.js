fb212839902920575c4f54e9464da5e865d994e1
Alexander Khrustalev <st.alex.ser@gmail.com>
2026-03-11 21:10:51 +0100

    Update lp.js

--- lp.js
+++ lp.js
@@ -1,5 +1,5 @@
 (function () {
     'use strict';
     // Загружаем основной код без блокировки интерфейса
-    Lampa.Utils.putScriptAsync(['https://raw.githubusercontent.com/alex-khrust/lp/main/tv_show.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://cdn.jsdelivr.net/gh/alex-khrust/lp@main/tv_show.js'], function () { });
 })();
