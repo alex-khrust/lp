95784c32b058075c2a7084a91cbad9375b1e3fc6
Alexander Khrustalev <st.alex.ser@gmail.com>
2026-03-11 17:26:16 +0100

    Update show.js

--- show.js
+++ show.js
@@ -5,13 +5,14 @@
         if (window.plugin_show_tv_ready) return;
         window.plugin_show_tv_ready = true;
 
-        // Самый простой способ добавить пункт, который точно будет кликабельным
+        // Добавляем пункт в меню. Используем нативный метод Lampa.Menu.add
         Lampa.Menu.add({
             title: 'ТВ Шоу',
             icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z" fill="#32CD32"/></svg>',
             onSelect: function () {
+                // При нажатии открывается поиск Lampa для нахождения шоу
                 Lampa.Input.edit({
-                    title: 'Поиск ТВ-шоу',
+                    title: 'Поиск ТВ-шоу (Россия, Украина)',
                     value: '',
                     free: true
                 }, function (new_value) {
@@ -25,9 +26,10 @@
                     }
                 });
             }
-        }, 3); // Цифра 3 поставит пункт повыше в меню
+        });
     }
 
+    // Инициализация плагина только когда приложение готово
     if (window.appready) startPlugin();
     else {
         Lampa.Listener.follow('app', function (e) {
