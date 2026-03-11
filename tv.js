94d893be9244f2f1d8f1a73e19695f282b4f15b4
Alexander Khrustalev <st.alex.ser@gmail.com>
2026-03-11 20:10:54 +0100

    Update lp.js

--- tv.js
+++ tv.js
@@ -1,31 +1,5 @@
 (function () {
     'use strict';
-
-    function startPlugin() {
-        if (window.plugin_tv_simple_ready) return;
-        window.plugin_tv_simple_ready = true;
-
-        // Самый простой и надежный способ добавления в меню
-        Lampa.Menu.add({
-            title: 'ТВ Шоу',
-            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z" fill="#32CD32"/></svg>',
-            onSelect: function () {
-                Lampa.Activity.push({
-                    url: '',
-                    title: 'ТВ Шоу',
-                    component: 'search',
-                    query: '',
-                    source: 'tmdb'
-                });
-            }
-        });
-    }
-
-    // Ждем полной готовности приложения
-    if (window.appready) startPlugin();
-    else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') startPlugin();
-        });
-    }
+    // Загружаем основной движок поиска через асинхронный метод Lampa
+    Lampa.Utils.putScriptAsync(['https://raw.githubusercontent.com/alex-khrust/lp/main/tv_engine.js'], function () { });
 })();
