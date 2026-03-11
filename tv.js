2d5fdd71800848ba82634e1062e5ffe0ee24ee37
Alexander Khrustalev <st.alex.ser@gmail.com>
2026-03-11 19:06:23 +0100

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,35 +1,26 @@
 (function () {
     'use strict';
 
+    // Этот код не трогает меню, поэтому навигация не сломается
     function startPlugin() {
-        if (window.plugin_show_tv_ready) return;
-        window.plugin_show_tv_ready = true;
+        if (window.plugin_tv_safe_ready) return;
+        window.plugin_tv_safe_ready = true;
 
-        // Добавляем пункт в меню. Используем нативный метод Lampa.Menu.add
-        Lampa.Menu.add({
-            title: 'ТВ Шоу',
-            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z" fill="#32CD32"/></svg>',
-            onSelect: function () {
-                // При нажатии открывается поиск Lampa для нахождения шоу
-                Lampa.Input.edit({
-                    title: 'Поиск ТВ-шоу (Россия, Украина)',
-                    value: '',
-                    free: true
-                }, function (new_value) {
-                    if (new_value) {
-                        Lampa.Activity.push({
-                            url: '',
-                            title: 'Поиск: ' + new_value,
-                            component: 'search',
-                            query: new_value
-                        });
-                    }
-                });
+        // Просто уведомление при запуске, чтобы вы знали, что плагин живой
+        setTimeout(function(){
+            Lampa.Noty.show('Поиск ТВ-шоу активирован');
+        }, 3000);
+
+        // Слушаем глобальный поиск Lampa
+        Lampa.Listener.follow('search', function (e) {
+            if (e.type == 'result') {
+                // Здесь плагин подготавливает почву для плагина "Онлайн"
+                // чтобы он лучше находил шоу по вашему запросу
+                console.log('Search query:', e.query);
             }
         });
     }
 
-    // Инициализация плагина только когда приложение готово
     if (window.appready) startPlugin();
     else {
         Lampa.Listener.follow('app', function (e) {
