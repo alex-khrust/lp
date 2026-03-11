8b9d7d915209c894c4921d11d8a1c4d3f8669827
Alexander Khrustalev <st.alex.ser@gmail.com>
2026-03-11 15:13:40 +0100

    Update kinobase_max.js

--- kinobase_max.js
+++ kinobase_max.js
@@ -1,8 +1,8 @@
 (function () {
     'use strict';
 
-    // Основной компонент плагина
-    function Kinobase(object) {
+    // 1. Создаем основной компонент (экран), который будет показывать контент
+    function KinobaseComponent(object) {
         var network = new Lampa.Reguest();
         var scroll  = new Lampa.Scroll({mask: true, over: true});
         var items   = [];
@@ -10,34 +10,32 @@
         
         this.create = function () {
             var _this = this;
-            this.activity.loader(true);
+            this.activity.loader(true); // Показываем индикатор загрузки
 
-            // Здесь в будущем будет fetch(object.url) и парсинг HTML
-            // Пока создаем демонстрационную карточку, чтобы убедиться, что всё работает
+            // Имитация получения данных (сюда позже добавим парсер HTML)
             setTimeout(function() {
-                var dummyData = [
-                    {
-                        title: 'Kinobase: ' + object.title,
-                        description: 'Для отображения реальных данных с ' + object.url + ' требуется написать парсер HTML.',
-                        card_id: 'kb_info'
-                    }
-                ];
-                _this.build(dummyData);
+                var card_data = {
+                    title: 'Kinobase: ' + object.title,
+                    description: 'Раздел ' + object.title + ' успешно подключен. Для отображения реальных карточек с сайта ' + object.url + ' необходимо добавить парсер контента.',
+                    card_id: 'kb_info'
+                };
+                
+                _this.build([card_data]);
             }, 500);
 
             return this.render();
         };
 
+        // Метод отрисовки карточек
         this.build = function (data) {
             var _this = this;
-            Lampa.Background.empty(); // Очищаем фон
+            Lampa.Background.empty();
 
             data.forEach(function (item) {
-                // Используем стандартный шаблон карточки Lampa
                 var card = Lampa.Template.get('card', item);
                 
                 card.on('hover:focus', function () {
-                    Lampa.Background.change(item.img); // Меняем фон при наведении
+                    // Можно менять фон при наведении
                 });
 
                 card.on('click:select', function () {
@@ -48,7 +46,7 @@
             });
 
             this.activity.loader(false);
-            this.activity.toggle(); // Переключаем фокус на контент
+            this.activity.toggle(); // Передаем управление интерфейсу
         };
 
         this.render = function () {
@@ -62,9 +60,49 @@
         };
     }
 
-    // Функция инициализации
+    // 2. Функция инициализации плагина
     function startPlugin() {
+        // Проверка, чтобы не загружать плагин дважды
+        if (window.plugin_kinobase_ready) return;
         window.plugin_kinobase_ready = true;
+
+        // Регистрируем компонент в системе Lampa
+        Lampa.Component.add('kinobase_max_component', KinobaseComponent);
+
+        // Массив разделов для добавления в меню
+        var rows = [
+            { title: 'KB: Фильмы', url: 'https://kinobase.org/films' },
+            { title: 'KB: Сериалы', url: 'https://kinobase.org/serials' },
+            { title: 'KB: Популярное', url: 'https://kinobase.org/popular' }
+        ];
+
+        // Добавляем каждый пункт в меню
+        rows.forEach(function (row) {
+            var menu_item = {
+                title: row.title,
+                icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z" fill="white"/></svg>',
+                onSelect: function () {
+                    // При клике открываем созданный выше компонент
+                    Lampa.Activity.push({
+                        url: row.url,
+                        title: row.title,
+                        component: 'kinobase_max_component',
+                        page: 1
+                    });
+                }
+            };
+            Lampa.Menu.add(menu_item);
+        });
+    }
+
+    // 3. Точка входа: ждем готовности приложения Lampa
+    if (window.appready) startPlugin();
+    else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type == 'ready') startPlugin();
+        });
+    }
+})();
 
         // Регистрируем компонент в системе
         Lampa.Component.add('kinobase_component', Kinobase);
