e296752f5a55ad5be20db12f07398416c428c2b2
Alexander Khrustalev <st.alex.ser@gmail.com>
2026-03-11 17:05:14 +0100

    Update show.js

--- show.js
+++ show.js
@@ -2,17 +2,16 @@
     'use strict';
 
     function startPlugin() {
-        if (window.plugin_tv_shows_ready) return;
-        window.plugin_tv_shows_ready = true;
+        if (window.plugin_show_tv_ready) return;
+        window.plugin_show_tv_ready = true;
 
-        // Добавляем пункт в главное меню
+        // Создаем кнопку в меню
         Lampa.Menu.add({
             title: 'ТВ Шоу',
             icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z" fill="#32CD32"/></svg>',
             onSelect: function () {
-                // При нажатии открывается поиск Lampa
                 Lampa.Input.edit({
-                    title: 'Поиск ТВ-шоу (МастерШеф, Пацанки и др.)',
+                    title: 'Название шоу (RU/UA)',
                     value: '',
                     free: true
                 }, function (new_value) {
