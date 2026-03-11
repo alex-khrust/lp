0c7c2b7ec8659ab9b10cfc0fa9b8f6207df43221
Alexander Khrustalev <st.alex.ser@gmail.com>
2026-03-11 15:28:04 +0100

    Update kinobase_max.js

--- kinobase_max.js
+++ kinobase_max.js
@@ -1,58 +1,39 @@
 (function () {
     'use strict';
 
-    // 1. Создаем основной компонент (экран), который будет показывать контент
     function KinobaseComponent(object) {
         var network = new Lampa.Reguest();
         var scroll  = new Lampa.Scroll({mask: true, over: true});
-        var items   = [];
         var html    = $('<div></div>');
         
         this.create = function () {
             var _this = this;
-            this.activity.loader(true); // Показываем индикатор загрузки
+            this.activity.loader(true);
 
-            // Имитация получения данных (сюда позже добавим парсер HTML)
+            // Используем прокси для обхода блокировок в DE
+            var url = 'https://proxy.cub.watch/https://kinobase.org/'; 
+            
+            // Тестовый вывод, чтобы проверить, работает ли экран
             setTimeout(function() {
-                var card_data = {
-                    title: 'Kinobase: ' + object.title,
-                    description: 'Раздел ' + object.title + ' успешно подключен. Для отображения реальных карточек с сайта ' + object.url + ' необходимо добавить парсер контента.',
-                    card_id: 'kb_info'
-                };
+                var card = Lampa.Template.get('card', {
+                    title: 'Kinobase в Германии',
+                    description: 'Если вы видите эту карточку, значит плагин загрузился. Нажмите на неё для теста.',
+                    card_id: 'kb_test'
+                });
                 
-                _this.build([card_data]);
-            }, 500);
+                card.on('click:select', function(){
+                    Lampa.Noty.show('Соединение установлено!');
+                });
+
+                html.append(card);
+                _this.activity.loader(false);
+                _this.activity.toggle();
+            }, 1000);
 
             return this.render();
         };
 
-        // Метод отрисовки карточек
-        this.build = function (data) {
-            var _this = this;
-            Lampa.Background.empty();
-
-            data.forEach(function (item) {
-                var card = Lampa.Template.get('card', item);
-                
-                card.on('hover:focus', function () {
-                    // Можно менять фон при наведении
-                });
-
-                card.on('click:select', function () {
-                    Lampa.Noty.show('Вы выбрали: ' + item.title);
-                });
-
-                html.append(card);
-            });
-
-            this.activity.loader(false);
-            this.activity.toggle(); // Передаем управление интерфейсу
-        };
-
-        this.render = function () {
-            return html;
-        };
-
+        this.render = function () { return html; };
         this.destroy = function () {
             network.clear();
             scroll.destroy();
@@ -60,79 +41,26 @@
         };
     }
 
-    // 2. Функция инициализации плагина
     function startPlugin() {
-        // Проверка, чтобы не загружать плагин дважды
         if (window.plugin_kinobase_ready) return;
         window.plugin_kinobase_ready = true;
 
-        // Регистрируем компонент в системе Lampa
-        Lampa.Component.add('kinobase_max_component', KinobaseComponent);
+        Lampa.Component.add('kinobase_max', KinobaseComponent);
 
-        // Массив разделов для добавления в меню
-        var rows = [
-            { title: 'KB: Фильмы', url: 'https://kinobase.org/films' },
-            { title: 'KB: Сериалы', url: 'https://kinobase.org/serials' },
-            { title: 'KB: Популярное', url: 'https://kinobase.org/popular' }
-        ];
-
-        // Добавляем каждый пункт в меню
-        rows.forEach(function (row) {
-            var menu_item = {
-                title: row.title,
-                icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z" fill="white"/></svg>',
-                onSelect: function () {
-                    // При клике открываем созданный выше компонент
-                    Lampa.Activity.push({
-                        url: row.url,
-                        title: row.title,
-                        component: 'kinobase_max_component',
-                        page: 1
-                    });
-                }
-            };
-            Lampa.Menu.add(menu_item);
+        Lampa.Menu.add({
+            title: 'KB: Фильмы',
+            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z" fill="white"/></svg>',
+            onSelect: function () {
+                Lampa.Activity.push({
+                    url: 'https://kinobase.org/films',
+                    title: 'KB: Фильмы',
+                    component: 'kinobase_max',
+                    page: 1
+                });
+            }
         });
     }
 
-    // 3. Точка входа: ждем готовности приложения Lampa
-    if (window.appready) startPlugin();
-    else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') startPlugin();
-        });
-    }
-})();
-
-        // Регистрируем компонент в системе
-        Lampa.Component.add('kinobase_component', Kinobase);
-
-        // Список разделов
-        var sections = [
-            { title: 'KB: Фильмы', url: 'https://kinobase.org/films' },
-            { title: 'KB: Сериалы', url: 'https://kinobase.org/serials' },
-            { title: 'KB: Популярное', url: 'https://kinobase.org/popular' }
-        ];
-
-        // Добавляем разделы в главное меню Lampa
-        sections.forEach(function (section) {
-            var menu_item = {
-                title: section.title,
-                icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="3" width="18" height="18" rx="2" stroke="white" stroke-width="2"/></svg>',
-                onSelect: function () {
-                    Lampa.Activity.push({
-                        url: section.url,
-                        title: section.title,
-                        component: 'kinobase_component',
-                        page: 1
-                    });
-                }
-            };
-            Lampa.Menu.add(menu_item);
-        });
-    }
-
-    // Запуск при готовности приложения
     if (window.appready) startPlugin();
     else {
         Lampa.Listener.follow('app', function (e) {
